b7d19ac00d5622f21279b6ff5e87b6f7d563bee2
Sebi Cirdeiu <sebbcirdeiu@yahoo.com>
2023-06-14 17:33:59 +0300

    Finish Tdd practice exercises

--- test.js
+++ test.js
@@ -17,18 +17,18 @@ function reverse(string) {
     return reversed;
 }
 
-test("2+2=4", () => {
+/*test("2+2=4", () => {
     expect(calculator.sum(2,2)).toBe(4);
 })
 test("10-5=5", () => {
     expect(calculator.substract(10,5)).toBe(5);
 })
-test("9:3=3", () => {
+("9:3=3", () => {
     expect(calculator.devide(9,3)).toBe(3);
 })
 test("9*3=3", () => {
     expect(calculator.multiply(9,3)).toBe(27);
-})
+})*/
 
 const calculator = {
     sum: function add(a,b) { return a+b},
@@ -38,3 +38,29 @@ const calculator = {
 }
 
 
+
+
+test("min", () => {
+    expect(analizeArray([6,7,8,9,10]).min).toBe(6)
+})
+test("max", () => {
+    expect(analizeArray([6,7,8,9]).max).toBe(9)
+})
+test("average", () => {
+    expect(analizeArray([6,7,8,9]).average).toBe(7.5)
+})
+test("length", () => {
+    expect(analizeArray([6,7,8,9]).length).toBe(4)
+})
+
+
+
+
+const analizeArray = (array) => {
+    return   {
+            min: Math.min(...array),
+            max: Math.max(...array),
+            average: array.reduce((a,b) => a+b, 0) / array.length,
+            length: array.length
+    }
+}
